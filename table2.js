ec27a1b2b4bcb03818c07d00599b415b8ec333f8
k5275zz <71121027+k5275zz@users.noreply.github.com>
2020-10-20 18:35:47 +0900

    Add files via upload
    테이블 제이쿼리로 동작시키기

---
+++ table2.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+
+$(document).ready(function(){
+	
+	$("tr:odd").css("backgroundColor","violet").css("color","white")
+	$("tr:even").css("backgroundColor","Beige").css("color","BlueViolet")
+	$("tr:nth-child(1)").css("backgroundColor","blue").css("color","yellow")
+})
